22560e7e6154d6d5ed899b66e4a26bc5f2bc24a7
Daniel <dale@create.aau.dk>
2023-07-04 13:45:52 +0200

    test

--- link_dataset.py
+++ link_dataset.py
@@ -1,6 +1,21 @@
 import os
 import shutil
 import argparse
+import subprocess
+
+def sync_onedrive():
+    cmd = "onedrive --synchronize --single-directory autofish_groups --verbose"
+    returned_value = subprocess.call(cmd, shell=True)  # returns the exit code in unix
+    if returned_value != 0:
+        print("Could not sync onedrive. Exiting...")
+        exit()
+
+def remove_pulled_data_folder():
+    cmd = "rm -r data"
+    returned_value = subprocess.call(cmd, shell=True)  # returns the exit code in unix
+    if returned_value != 0:
+        print("Could not remove the pulled data folder. Exiting...")
+        exit()
 
 if __name__=="__main__":
     parser = argparse.ArgumentParser()
@@ -10,8 +25,16 @@ if __name__=="__main__":
     if args.path == None:
         print("Please, provide path to the dataset folder\n     python3 link_dataset.py -p path/to/dataset/")
         exit()
-    shutil.copytree(args.path, "data", copy_function=os.link)
 
+    #Remove pulled data folder 
+    if os.path.exists("data"):
+        remove_pulled_data_folder()
+
+    #Synch onedrive to get the latest dataset version before creating link
+    sync_onedrive()
+
+    #Create hardlink with the dataset on Onedrive
+    shutil.copytree(args.path, "data", copy_function=os.link)
     with open(os.path.join(args.output, ".gitignore"), "w") as f:
         f.write("group_*/calibration\n")
         f.write("group_*/heatmaps\n")
